516425da242991aa3be93dea53b465f8508f325e
dkurtaev <dmitry.kurtaev@gmail.com>
2016-08-17 22:41:02 +0300

    Shading by depth

--- src/furry_road/Surface.java
+++ src/furry_road/Surface.java
@@ -20,9 +20,10 @@ public class Surface {
         ShaderFactory.CreateShaderProgram(gl, "shaders/surface_shader.vertex",
                                           "shaders/surface_shader.fragment");
     loc_opacity_map = gl.glGetUniformLocation(shader_program, "u_opacity_map");
+    loc_layer_depth = gl.glGetUniformLocation(shader_program, "u_layer_depth");
   }
 
-  public void Draw(GL2 gl, float light_vector[], int layer_idx) {
+  public void Draw(GL2 gl, float light_vector[], int layer_idx, int n_layers) {
     gl.glUseProgram(shader_program);
     gl.glEnable(GL.GL_TEXTURE_2D);
     gl.glEnable(GL2.GL_BLEND);
@@ -30,6 +31,7 @@ public class Surface {
     gl.glEnable(GL2.GL_ALPHA_TEST);
 
     gl.glUniform1i(loc_opacity_map, 0);
+    gl.glUniform1f(loc_layer_depth, (float)layer_idx / (n_layers - 1));
 
     gl.glBindTexture(GL.GL_TEXTURE_2D, opacity_map_id);
     gl.glBegin(GL2.GL_QUADS);
@@ -48,7 +50,7 @@ public class Surface {
   public void GenerateOpacityMap(GL2 gl, int n_surfaces) {
     final int width = 256;
     final int height = 256;
-    final float fur_dencity = 0.5f;
+    final float fur_dencity = 0.1f;
 
     final int dim = width * height;
     final int n_seeds = (int)(dim * fur_dencity);
@@ -102,5 +104,6 @@ public class Surface {
 
   private int shader_program;
   private int loc_opacity_map;
+  private int loc_layer_depth;
 
 }
